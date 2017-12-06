39f9a99afe1e7834501e6c121d1a437bb752d151
Alan7305 <33473357+Alan7305@users.noreply.github.com>
2017-12-06 17:47:21 +0800

    1

---
+++ Controllers/StocksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NvestedExp.Controllers
+{
+    public class StocksController : Controller
+    {
+        // GET: Stocks
+        public ActionResult Index()
+        {
+            ViewBag.title = "Stocks";
+            ViewBag.titleCh = "股票投資總覽";
+            ViewBag.menuName = "2330台積電";
+            return View();
+        }
+    }
+}
